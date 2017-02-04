431859c61276abbe7cfe94e861006949851635ce
Sofiya Semenova <novasofiya@gmail.com>
2017-02-04 14:07:58 -0500

    Stitched

---
+++ script.js
@@ -0,0 +1,14 @@
+var lastTime;
+
+function setUpCanvas() {
+    var canvas = document.getElementById('art');
+    var ctx = canvas.getContext('2d');
+}
+
+function startDrawing() {
+    
+}
+
+function draw() {
+
+}
